7652a6e47566ba71ce548d9c9b205a8a1646877e
JakubPloch <kubaploch34@gmail.com>
2023-07-01 10:38:33 +0200

    Add temporary grid of cards

--- frontend/album-front/src/app/pages/cards/cards-grid/cards-grid.component.ts
+++ frontend/album-front/src/app/pages/cards/cards-grid/cards-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, startWith, switchMap } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-cards-grid',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards-grid.component.css']
 })
 export class CardsGridComponent {
+cardsAmount = new Array(10).fill('hello');
+
+constructor(protected api: ApiService) {
+
+}
+
+refresh = new BehaviorSubject<boolean>(false);
+request$ = this.refresh.pipe(startWith(true), switchMap(x => this.api.get('/trading-card')));
+
+
+ngOnInit(): void {
+  console.log(this.request$);
+}
+
 
 }
